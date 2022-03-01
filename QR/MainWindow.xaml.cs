d7a8e73b96c8ccbce4b0429909879c92ad82349a
jiri.vimmer <jiri.vimmer@VT2-04>
2022-03-01 09:01:33 +0100

    Přidejte soubory projektu.

---
+++ QR/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using QRCoder;
+using QRCoder.Xaml;
+
+namespace QR
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Generate_Click(object sender, RoutedEventArgs e)
+        {
+            string textZadan = Text.Text;
+            QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+
+            QRCodeData dataQR = qRCodeGenerator.CreateQrCode(textZadan, QRCodeGenerator.ECCLevel.M);
+
+            XamlQRCode qrcode = new XamlQRCode(dataQR);
+
+            DrawingImage qrcoder = qrcode.GetGraphic(100);
+            obrazek.Source = qrcoder;
+        }
+    }
+}
